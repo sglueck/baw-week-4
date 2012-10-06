bd2f97d682de2c032ff82ea9dcdafe81160f8480
sglueck <sglueck1@gmail.com>
2012-10-06 12:26:15 -0400

    another commit

--- Exercise1.php
+++ Exercise1.php
@@ -2,7 +2,11 @@
 $name="Susan";
 $birthdate="February 20, 1964";
 $favcolor="Blue";
-print "My name is $name\n";
-print "I was born $birthdate\n";
-print "My favorite color is $favcolor\n";
+
+$br = "<br>";
+print "Homework for Saturday, October 6, 2012: Exercise 1" . $br . $br;
+print "My name is $name.$br";
+print "I was born $birthdate.$br";
+print "My favorite color is $favcolor.$br" . $br;
+print "I'm going to Utica College!";
 ?>
--- Exercise2.php
+++ Exercise2.php
@@ -4,11 +4,24 @@
 -- $DaysPerWeek
 -- $HoursPerDay
 -- $MinutesPerHour
+
+How many minutes are there in a week in the Bilky Way, a near parallel galaxy,
+if there are 26 hours on a day there? 
 */
+$br = "<br>";
+$sp = " ";
 
 $DaysPerWeek = 7;
 $HoursPerDay = 24;
 $MinutesPerHour = 60;
-print "The number of minutes in a week is $DaysPerWeek * $HoursPerDay * $MinutesPerHour\n";
+$Total = $DaysPerWeek * $HoursPerDay * $MinutesPerHour;
+
+
+print "Homework for Saturday, October 6, 2012: Exercise 2" . $br . $br;
+print "The number of minutes in a week is $DaysPerWeek * $HoursPerDay * $MinutesPerHour$sp=$sp". $Total . $br . $br;
+
+$HoursPerDay = 26; /* Bilky way has long days */
+$Total = $DaysPerWeek * $HoursPerDay * $MinutesPerHour;
+print "The number of minutes in a week in the Bilky Way is $DaysPerWeek * $HoursPerDay * $MinutesPerHour$sp=$sp" . $Total . $br;
 
 ?>
