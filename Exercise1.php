f436a20810130b091b61062aac86793c8c82a23f
sglueck <sglueck1@gmail.com>
2012-10-06 10:47:20 -0400

    another commit

---
+++ Exercise1.php
@@ -0,0 +1,8 @@
+<?php
+$name="Susan";
+$birthdate="February 20, 1964";
+$favcolor="Blue";
+print "My name is $name\n";
+print "I was born $birthdate\n";
+print "My favorite color is $favcolor\n";
+?>
---
+++ Exercise2.php
@@ -0,0 +1,14 @@
+<?php
+
+/* Calculate the number of minutes in a week using variables
+-- $DaysPerWeek
+-- $HoursPerDay
+-- $MinutesPerHour
+*/
+
+$DaysPerWeek = 7;
+$HoursPerDay = 24;
+$MinutesPerHour = 60;
+print "The number of minutes in a week is $DaysPerWeek * $HoursPerDay * $MinutesPerHour\n";
+
+?>
